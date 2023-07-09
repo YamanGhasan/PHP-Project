38c1a4a6d2b05e92a6762db70c65f3ce2da40886
YamanGhasan <116804915+YamanGhasan@users.noreply.github.com>
2023-07-09 23:11:58 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,20 @@
 </head>
 
 <body>
+ \
     <div id="search-bar">
         <h1>IMDB</h1>
         <form id="search-form" method="GET" action="{{ route('movies.search') }}">
             <input type="text" name="query" id="search-input" placeholder="Search IMDB" required/>
         </form>
-        <button type="submit" class="watch-video-button"></button>
-        <button type="submit" class="watch-video-button"></button>
+        <span class="username">
+   <h1>
+        <a href="{{ route('profile') }}">{{ session('username') }} Profile👤</a> 
+</h1>
+</span>
+
+        <!-- <button type="submit" class="watch-video-button"></button> -->
+        <!-- <button type="submit" class="watch-video-button"></button> -->
     </div>
   
     <section class="photo-section">
@@ -24,10 +31,7 @@
         <button type="submit" class="watch-video-button">Watch Video!</button>
     </form>
 </section>
-
-
-<div id="video-container"></div>
-<section id="movies-section" class="hidden">
+ 
     @if ($movies && count($movies) > 0)
         <h1 id="title">Popular Movies</h1>
         <div id="movies-grid">
@@ -38,12 +42,17 @@
                     <div class="movie-detail">
                         @if (isset($movie['release_date']))
                         <?php $releaseDate = new DateTime($movie['release_date']); ?>
-                        <span>Release Date:</span> {{ $releaseDate->format('Y') }}
+                        <span> Release Date:  {{ $releaseDate->format('Y') }}</span>
                             @endif
-                           
-    <br>
-    <br>
-                        <span class="vote-average">{{ $movie['vote_average'] }}</span> <span>⭐</span> 
+                
+<br> <br>
+                        <span class="vote-average">{{ $movie['vote_average'] }}⭐</span> 
+                        <br> <br>
+                        <form method="POST" action="{{ route('movies.favorite', ['id' => $movie['id'], 'type' => 'movie']) }}">
+    @csrf
+    <button type="submit" class="add-favorite-btn">Add to Favorite</button>
+</form>
+
                     </div>
                 </div>
             @endforeach
@@ -51,8 +60,8 @@
     @else
         <p>No movies found.</p>
     @endif
-     </section>
-    <section id="tv-shows-section" class="hidden">
+   
+    <!-- <section id="tv-shows-section" class="hidden"> -->
     @if ($TvShows && count($TvShows) > 0)
         <h1 id="title">Popular TV Shows</h1>
         <div id="movies-grid">
@@ -60,7 +69,6 @@
                 <div class="movie-card">
                     <img src="https://image.tmdb.org/t/p/w500{{ $tvShow['poster_path'] }}" class="movie-poster" alt="{{ $tvShow['original_name'] }} Poster">
                     <h2 class="movie-title"><a href="{{ route('tvshow.show', ['id' => $tvShow['id']]) }}">{{ $tvShow['name'] }}</a></h2>
-
                     <div class="movie-detail">
           @if (isset($tvShow['first_air_date']))
                 <?php $firstAirDate = new DateTime($tvShow['first_air_date']); ?>
@@ -68,7 +76,14 @@
 @endif
 <br>
                         <br>
-                       <span class="vote-average">{{ $tvShow['vote_average'] }}</span> <span>⭐</span> 
+                       <span class="vote-average">{{ $tvShow['vote_average'] }}</span> <span>⭐</span>
+                       <br> <br>
+                         <!-- Add to Favorite button -->
+                         <form method="POST" action="{{ route('movies.favorite', ['id' => $movie['id'], 'type' => 'tv']) }}">
+    @csrf
+    <button type="submit" class="add-favorite-btn">Add to Favorite</button>
+</form>
+
                     </div>
                 </div>
             @endforeach
