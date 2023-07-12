1a1309f7bd9b16f5cc745b92383a911b5f5d1b22
YamanGhasan <s12028365@stu.najah.edu>
2023-07-12 18:04:15 +0300

    profile design

--- storage/framework/views/60bc211bb9da0837564c3b32f43a0391.php
+++ storage/framework/views/60bc211bb9da0837564c3b32f43a0391.php
@@ -1,5 +1,4 @@
-
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
@@ -23,6 +22,7 @@
         text-align: center;
         background-color: #f1f1f1;
         border-radius: 10px;
+        /* background-color : gray; */
     }
 
     .movie-poster {
@@ -30,40 +30,66 @@
         height: auto;
         margin-bottom: 10px;
     }
+  
 
+    .top-right {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+
+    .logout-btn {
+        margin-left: 10px;
+    }
     .movie-details {
         font-size: 16px;
         color: #333;
     }
-     
-    h1 {
-        font-size: 24px;
-        color: #333;
-        text-align: center;
-        /* margin-bottom: 20px; */
-       
-    }
- 
+    .add-favorite-btn{
+    font-family:  Arial, Helvetica, sans-serif;
+    color: black;
+    text-decoration: none;
+    background-color: #f4c518;
+    font-size: 20px;
+    margin: 10px;
+    border-radius: 4px;
+  }
+  .btns{
+    font-family:  Arial, Helvetica, sans-serif;
+    color: black;
+    text-decoration: none;
+    background-color: #f4c518;
+    font-size: 20px;
+    margin: 10px;
+    border-radius: 4px;
+    font-size:30px;
+  }
 </style>
 </head>
 <body>
-<section class="photo-section">
-    <div class="blurry-background"></div>
 
 <div class="card">
-
-<svg width="240" height="240" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/> <path d="M4.271 18.3457C4.271 18.3457 6.50002 15.5 12 15.5C17.5 15.5 19.7291 18.3457 19.7291 18.3457" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/> <path d="M12 12C13.6569 12 15 10.6569 15 9C15 7.34315 13.6569 6 12 6C10.3431 6 9 7.34315 9 9C9 10.6569 10.3431 12 12 12Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/> </svg>
-<h1>
-        <a href="<?php echo e(route('profile.logout')); ?>" class="logout-btn">Logout   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg></a>
+<div class="top-right">
       
-</h1>
-  <!-- <img src="/w3images/team2.jpg" alt="John" style="width:100%"> -->
-  <h1>User Name:  <span><?php echo e($user->username); ?> </span> </h1>
-  <p>Age: <?php echo e($user->age); ?></p><h1 class="myfavh1">My Favorites</h1>
+      <a href="<?php echo e(route('profile.logout')); ?>" class="btns">
+          Logout
+          <svg xmlns="http://www.w3.org/2000/svg" width="15" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out">
+              <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+              <polyline points="16 17 21 12 16 7"></polyline>
+              <line x1="21" y1="12" x2="9" y2="12"></line>
+          </svg>
+      </a>
+  
   <div>
-            <a href="<?php echo e(route('movies.index')); ?>" class="btn btn-primary">Go to homepage</a>
-        </div>
+      <a href="<?php echo e(route('movies.index')); ?>" class="btns" >Go to homepage</a>
+  </div>
 </div>
+<svg width="240" height="240" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/> <path d="M4.271 18.3457C4.271 18.3457 6.50002 15.5 12 15.5C17.5 15.5 19.7291 18.3457 19.7291 18.3457" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/> <path d="M12 12C13.6569 12 15 10.6569 15 9C15 7.34315 13.6569 6 12 6C10.3431 6 9 7.34315 9 9C9 10.6569 10.3431 12 12 12Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/> </svg>
+<h1>User Name:  <span><?php echo e($user->username); ?> </span> </h1>
+<h1>Age:  <span><?php echo e($user->age); ?></span> </h1>
+ 
+  <h1 class="myfavh1">My Favorites</h1>
+  
 
 
 <div class="favorite-section">
@@ -76,7 +102,7 @@
             <form action="<?php echo e(route('remove_favorite', ['id' => $movie['id']])); ?>" method="POST">
         <?php echo csrf_field(); ?>
         <?php echo method_field('DELETE'); ?>
-        <button type="submit" class="logout-btn">Remove from Favorites</button>
+        <button type="submit"  class="add-favorite-btn">Remove from Favorites</button>
     </form>
 
         </div>
@@ -93,7 +119,7 @@
             <form action="<?php echo e(route('remove_favorite', ['id' => $tvShow['id']])); ?>" method="POST">
                 <?php echo csrf_field(); ?>
                 <?php echo method_field('DELETE'); ?>
-                <button type="submit" class="logout-btn">Remove from Favorites</button>
+                <button type="submit"  class="add-favorite-btn">Remove from Favorites</button>
             </form>
         </div>
     <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
