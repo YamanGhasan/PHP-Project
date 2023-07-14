f8a7abc7ff56d1574aa68c22cffb41199ad0f216
YamanGhasan <116804915+YamanGhasan@users.noreply.github.com>
2023-07-14 23:44:25 +0300

    Update MovieController.php

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Request; 
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Http; 
 use Illuminate\Support\Facades\Auth;
