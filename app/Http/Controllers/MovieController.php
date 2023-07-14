5719704e85ae5b410f294be27b3682fded6561ad
YamanGhasan <116804915+YamanGhasan@users.noreply.github.com>
2023-07-14 23:44:15 +0300

    Update MovieController.php

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
-use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Http; 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Models\Favorite;
