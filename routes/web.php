0739e7dad1981b2c15d9273f854dcca69efe087d
YamanGhasan <s12028365@stu.najah.edu>
2023-07-18 23:26:04 +0300

    clean code

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,8 @@ Route::post('/tvshows/{id}/favorite', [MovieController::class, 'addToFavoritesTv
 Route::delete('/favorites/remove/{id}', [MovieController::class, 'remove'])->name('remove_favorite');
 
  
+// Route::delete('/favorites/remove/{id}', [MovieController::class, 'remove'])->name('remove_favorite');
+
 
 
 
